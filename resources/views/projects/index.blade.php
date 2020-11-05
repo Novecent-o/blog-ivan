76ca08ecfad442f4e836b6f0178d93d002c9686d
Novecento <ivan.monfrini@gmail.com>
2020-11-05 10:49:44 +0100

    Auto stash before merge of "main" and "origin/main"

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,12 +9,17 @@
     <div class="row">
       <div class="card-deck">
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
               <div class="">
                 <img src="/img/boolflix.png" class="card-img-top p-3" alt="...">
               </div>
             </a>
+=======
+          <div class="card m-5">
+            <img src="/img/boolflix.png" class="card-img-top" alt="...">
+>>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">BoolFlix</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -23,12 +28,17 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="">
               <div class="">
                 <img src="/img/boolBNB.png" class="card-img-top p-3" alt="...">
               </div>
             </a>
+=======
+          <div class="card m-5">
+            <img src="/img/boolBNB.png" class="card-img-top" alt="...">
+>>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">AirBnb</h5>
               <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -37,12 +47,17 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="https://novecent-o.github.io/html-css-digitalocean/">
               <div class="">
                 <img src="/img/digitalOcean.png" class="card-img-top p-3" alt="...">
               </div>
             </a>
+=======
+          <div class="card m-5">
+            <img src="/img/digitalOcean.png" class="card-img-top" alt="...">
+>>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">DigitalOcean</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -51,12 +66,17 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
               <div class="">
                 <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="...">
               </div>
             </a>
+=======
+          <div class="card m-5">
+            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
+>>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">Spotify Web</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -65,12 +85,17 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
               <div class="">
                 <img src="/img/whatsapp.png" class="card-img-top p-3" alt="...">
               </div>
             </a>
+=======
+          <div class="card m-5">
+            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
+>>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">Whatsapp</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -79,10 +104,27 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
+<<<<<<< Updated upstream
           <div class="card mb-4">
             <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="...">
             <div class="card-body">
               <h5 class="card-title">BoolFlix</h5>
+=======
+          <div class="card m-5">
+            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card m-5">
+            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+>>>>>>> Stashed changes
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
