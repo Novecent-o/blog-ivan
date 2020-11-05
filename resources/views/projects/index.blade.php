6afbcb0e4eb42cf0e6090fe4fb5b5832ecf40fac
Ivan <ivan.monfrini@gmail.com>
2020-11-05 23:36:12 +0100

    siamo online

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,36 +9,26 @@
     <div class="row">
       <div class="card-deck">
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
-              <div class="">
-                <img src="/img/boolflix.png" class="card-img-top p-3" alt="...">
+              <div class="pappo">
+                <img src="/img/boolflix.png" class="card-img-top p-3" alt="Boolflix">
               </div>
             </a>
-=======
-          <div class="card m-5">
-            <img src="/img/boolflix.png" class="card-img-top" alt="...">
->>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">BoolFlix</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <p class="card-text">Riproduzione del funzionamento di ricerca della nota società Netflix, con una chiamata Ajax ho interrogato il database del sito "The Movie DB" per stampare i risultati di ricerca corrispondenti</p>
+              <p class="card-text"><small class="text-muted">JavaScript - HTML5 - CSS3 - Rest-API</small></p>
             </div>
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="">
-              <div class="">
-                <img src="/img/boolBNB.png" class="card-img-top p-3" alt="...">
+              <div class="pappo">
+                <img src="/img/boolBNB.png" class="card-img-top p-3" alt="AirBnb">
               </div>
             </a>
-=======
-          <div class="card m-5">
-            <img src="/img/boolBNB.png" class="card-img-top" alt="...">
->>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">AirBnb</h5>
               <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -47,17 +37,12 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="https://novecent-o.github.io/html-css-digitalocean/">
-              <div class="">
-                <img src="/img/digitalOcean.png" class="card-img-top p-3" alt="...">
+              <div class="pappo">
+                <img src="/img/digitalOcean.png" class="card-img-top p-3" alt="Digital Ocean">
               </div>
             </a>
-=======
-          <div class="card m-5">
-            <img src="/img/digitalOcean.png" class="card-img-top" alt="...">
->>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">DigitalOcean</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -66,17 +51,12 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
-              <div class="">
-                <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="...">
+              <div class="pappo">
+                <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="Spotify Web">
               </div>
             </a>
-=======
-          <div class="card m-5">
-            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
->>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">Spotify Web</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -85,17 +65,12 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
             <a href="#">
-              <div class="">
-                <img src="/img/whatsapp.png" class="card-img-top p-3" alt="...">
+              <div class="pappo">
+                <img src="/img/whatsapp.png" class="card-img-top p-3" alt="Whatsapp Web">
               </div>
             </a>
-=======
-          <div class="card m-5">
-            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
->>>>>>> Stashed changes
             <div class="card-body">
               <h5 class="card-title">Whatsapp</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -104,27 +79,14 @@
           </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-4">
-<<<<<<< Updated upstream
           <div class="card mb-4">
-            <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="...">
+            <a href="#">
+              <div class="pappo">
+                <img src="/img/spotifyWeb.png" class="card-img-top p-3" alt="...">
+              </div>
+            </a>
             <div class="card-body">
               <h5 class="card-title">BoolFlix</h5>
-=======
-          <div class="card m-5">
-            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4">
-          <div class="card m-5">
-            <img src="/img/spotifyWeb.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
->>>>>>> Stashed changes
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
